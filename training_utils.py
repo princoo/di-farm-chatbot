53972cf64348dc543e45a6f999c3e2b1e8e049b3
princoo <princeineza@gmail.com>
2024-10-06 11:26:03 +0200

    refact(codebase):
    - removes old duplicates

--- training_utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch import nn
-
-def loss_fn() -> nn.Module:
-    return torch.nn.CrossEntropyLoss()
-
-def optimizer(model:nn.Module, learning_rate=0.001) -> torch.optim.Optimizer:
-    return torch.optim.Adam(model.parameters(), lr=learning_rate)
